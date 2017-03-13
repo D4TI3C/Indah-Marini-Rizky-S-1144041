7943aca2d41c071997f7bbd8b22c6febe5d8598c
IndahMarini <indah.marini.rizky@gmail.com>
2017-03-13 13:24:32 +0700

    pertemuan1

---
+++ doc/kuliah/tugas1.py
@@ -0,0 +1,194 @@
+import time
+start_time = time.time()
+print("Menghitung nilai rumus matematika menggunakan bahasa itali")
+i = raw_input("Masukan Nilai 1: ")
+n = raw_input("Masukan Nilai 2: ")
+d = raw_input("Masukan Nilai 3: ")
+a = raw_input("Masukan Nilai 4: ")
+h = raw_input("Masukan Nilai 5: ")
+
+if i == 'sada':
+	i=1
+
+if i == 'dua':
+	i=2
+
+if i == 'tolu':
+	i=3
+
+if i == 'opat':
+	i=4
+
+if i == 'lima':
+	i=5
+
+if i == 'onom':
+	i=6
+
+if i == 'pitu':
+	i=7
+
+if i == 'walu':
+	i=8
+
+if i == 'sia':
+	i=9
+
+if i == 'nol':
+	i=0
+
+
+if n == 'sada':
+	n=1
+
+if n == 'dua':
+	n=2
+
+if n == 'tolu':
+	n=3
+
+if n == 'opat':
+	n=4
+
+if n == 'lima':
+	n=5
+
+if n == 'onom':
+	n=6
+
+if n == 'pitu':
+	n=7
+
+if n == 'walu':
+	n=8
+
+if n == 'sia':
+	n=9
+
+if n == 'nol':
+	n=0
+
+if n == 'sada':
+	n=1
+
+if n == 'dua':
+	n=2
+
+if n == 'tolu':
+	n=3
+
+if n == 'opat':
+	n=4
+
+if n == 'lima':
+	n=5
+
+if n == 'onom':
+	n=6
+
+if n == 'pitu':
+	n=7
+
+if n == 'walu':
+	n=8
+
+if n == 'sia':
+	n=9
+
+if n == 'nol':
+	n=0
+
+if d == 'sada':
+	d=1
+
+if d == 'dua':
+	d=2
+
+if d == 'tolu':
+	d=3
+
+if d == 'opat':
+	d=4
+
+if d == 'lima':
+	d=5
+
+if d == 'onom':
+	d=6
+
+if d == 'pitu':
+	d=7
+
+if d == 'walu':
+	d=8
+
+if d == 'sia':
+	d=9
+
+if d == 'nol':
+	d=0
+
+if a == 'sada':
+	a=1
+
+if a == 'dua':
+	a=2
+
+if a == 'tolu':
+	a=3
+
+if a == 'opat':
+	a=4
+
+if a == 'lima':
+	a=5
+
+if a == 'onom':
+	a=6
+
+if a == 'pitu':
+	a=7
+
+if a == 'walu':
+	a=8
+
+if a == 'sia':
+	a=9
+
+if a == 'nol':
+	a=0
+
+
+if h == 'sada':
+	h=1
+
+if h == 'dua':
+	h=2
+
+if h == 'tolu':
+	h=3
+
+if h == 'opat':
+	h=4
+
+if h == 'lima':
+	h=5
+
+if h == 'onom':
+	h=6
+
+if h == 'pitu':
+	h=7
+
+if h == 'walu':
+	h=8
+
+if h == 'sia':
+	h=9
+
+if h == 'nol':
+	h=0
+
+jumlah =(i*n)+(d/a-h) 
+print ("hasil" , jumlah) 
+print("time : %s detik " % (time.time() - start_time))
